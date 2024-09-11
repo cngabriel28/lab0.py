a557f6178eac25c5a111f218f591decc2fad657b
cngabriel28 <cngabriel@loyola.edu>
2024-09-10 23:57:51 -0400

    Initial commit

---
+++ Lab0.py
@@ -0,0 +1,13 @@
+#Programmer: Cassandra Gabriel
+#Course: CS 151, Mr. John
+#Due date: 9/10/24
+#Problem statement: Output information about the programmers
+#Data input: None - there is no user input in this program
+#Data output: The name of the programmer and 5 statements about them
+
+print("CS151 Lab 0")
+print("Hi! My name is Cassandra Gabriel.")
+print("I am a computer science major and I'm also a commuter student.")
+print("I have three dogs named Chanel, Gucci, and Louie.")
+print("I used to work for Chili's.")
+print("My favorite color is pink and I love to travel!")
